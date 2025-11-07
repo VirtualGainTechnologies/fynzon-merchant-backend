37100cd4a9bf0ee212e5cc5cb0d47c00bbf73f01
Bhagwan <virtualgaintechnologies007@gmail.com>
2025-11-07 16:18:10 +0530

    .

--- controllers/merchant/authController.js
+++ controllers/merchant/authController.js
@@ -1,14 +1,14 @@
 const { sendOtpToEmail } = require("../../utils/sendOtp");
 const AppError = require("../../utils/AppError");
 
-exports.sendOtpEmail = async (req, res) => {
+exports.sendRegistrationOtp = async (req, res) => {
   const emailObject = {
     type: "register-otp",
     email: req.body.email,
     userName: req.body.userName,
     title: "Registration",
   };
-    const emailData = await sendOtpToEmail(emailObject);
+  const emailData = await sendOtpToEmail(emailObject);
   if (emailData.error) {
     throw new AppError(400, emailData.message);
   }
@@ -16,6 +16,11 @@ exports.sendOtpEmail = async (req, res) => {
   res.status(200).json({
     message: `OTP sent to email`,
     error: false,
-    data: emailData?.data,
+    data: {
+      otpId: emailData?.data.otpId,
+      ...req.body,
+    },
   });
-}
+};
+
+exports.verifyRegistrationOtp = () => {};
--- routers/merchant/authRouter.js
+++ routers/merchant/authRouter.js
@@ -1,26 +1,86 @@
 const { body } = require("express-validator");
 const router = require("express").Router();
 
-const { sendOtpEmail } = require("../../controllers/merchant/authController");
+const {
+  sendRegistrationOtp,
+  verifyRegistrationOtp,
+} = require("../../controllers/merchant/authController");
 const { catchAsync } = require("../../utils/catchAsync");
 
-const emailOtpValidator = [
-  body("userName")
+const sendRegistrationOtpValidator = [
+  body("category")
     .notEmpty()
-    .withMessage("Please provide user or business name"),
+    .trim()
+    .withMessage("Please provide category")
+    .isIn(["INDIVIDUAL", "ENTITY"])
+    .withMessage("Category must be either INDIVIDUAL or ENTITY"),
   body("email")
     .notEmpty()
-    .withMessage("Please provide email id")
     .trim()
+    .withMessage("Please provide email id")
     .isEmail()
     .withMessage("Invalid email id")
     .toLowerCase(),
+  body("password").notEmpty().trim().withMessage("Please provide password"),
+
+  body("businessName")
+    .if(body("category").custom((value) => value === "ENTITY"))
+    .notEmpty()
+    .trim()
+    .withMessage("Please provide business name")
+    .toLowerCase(),
+  body("businessCategory")
+    .if(body("category").custom((value) => value === "ENTITY"))
+    .notEmpty()
+    .trim()
+    .withMessage("Please provide business category"),
+
+  body("fullName")
+    .if(body("category").custom((value) => value === "INDIVIDUAL"))
+    .notEmpty()
+    .trim()
+    .withMessage("Please provide full name"),
+  body("profession")
+    .if(body("category").custom((value) => value === "INDIVIDUAL"))
+    .notEmpty()
+    .trim()
+    .withMessage("Please provide profession"),
+
+  body("phoneCode").notEmpty().trim().withMessage("Please provide phone code"),
+  body("phone")
+    .notEmpty()
+    .trim()
+    .withMessage("Please provide phone number")
+    .custom(async (val, { req }) => {
+      if (/^[6-9]{1}[0-9]{9}$/.test(val)) {
+        return true;
+      } else {
+        throw new Error("Invalid phone number");
+      }
+    }),
+];
+
+const verifyRegistrationOtpValidator = [
+  body("otpId").notEmpty().trim().withMessage("The field otpId is missing"),
+  body("otp")
+    .notEmpty()
+    .trim()
+    .withMessage("The field otp is missing")
+    .isLength({ min: 6, max: 6 })
+    .withMessage("OTP must be of 6 digit"),
+  ...sendRegistrationOtpValidator,
 ];
 
 router.post(
-  "/send-email-otp",
-  emailOtpValidator,
-  catchAsync("sendOtpToEmail api", sendOtpEmail)
+  "/registration/send-otp",
+  sendRegistrationOtpValidator,
+  catchAsync("sendRegistrationOtp api", sendRegistrationOtp)
+);
+
+router.post(
+  "/registration/verify-otp",
+  verifyRegistrationOtpValidator,
+  catchAsync("verifyRegistrationOtp api", verifyRegistrationOtp)
 );
 
 module.exports = router;
